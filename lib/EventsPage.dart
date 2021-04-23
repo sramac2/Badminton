6cfe0752a1d73880101ae7a0577f629d781cce9c
Santhosh ramachandran <ramnkl8219@gmail.com>
2021-04-23 16:58:45 -0400

    Added Events Page

---
+++ lib/EventsPage.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(CalendarApp()));
+}
+
+class CalendarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TableCalendar Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StartPage(),
+    );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  ValueNotifier<List<String>> _selectedEvents;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOff; // Can be toggled on/off by longpressing a date
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay;
+  DateTime _rangeStart;
+  DateTime _rangeEnd;
+
+  @override
+  void initState() {
+    _selectedEvents = ValueNotifier(_getEventsForDay(DateTime.now()));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Events'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+              calendarFormat: CalendarFormat.twoWeeks,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              eventLoader: (day) {
+                return _getEventsForDay(day);
+              },
+              onDaySelected: _onDaySelected,
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ValueListenableBuilder<List<String>>(
+                valueListenable: _selectedEvents,
+                builder: (context, value, _) {
+                  return ListView.builder(
+                    itemCount: value.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 4.0,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: ListTile(
+                          onTap: () => print('${value[index]}'),
+                          title: Text('${value[index]}'),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _rangeStart = null; // Important to clean those
+        _rangeEnd = null;
+        _rangeSelectionMode = RangeSelectionMode.toggledOff;
+      });
+    }
+    _selectedEvents.value = _getEventsForDay(selectedDay);
+  }
+
+  List<String> _getEventsForDay(DateTime day) {
+    // Implementation example
+    if (day.day == 20 ||
+        
+        day.day == 14 ||
+        day.day == 30 ||
+        day.day == 10) {
+      return ['5am', '7am', '9am', '11am'];
+    } else {
+      return [];
+    }
+  }
+}
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:badminton/CourtListingPage.dart';
+import 'package:badminton/EventsPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,51 +14,46 @@ class HomePage extends StatefulWidget {
 class HomeState extends State<HomePage> {
   static List<Widget> _widgetOptions = <Widget>[
     CourtListingPage(),
-    Text(
-      'Index 2: School',
-     // style: optionStyle,
-    ),
+    CalendarApp(),
     Text(
       'Index 3: School',
-     // style: optionStyle,
+      // style: optionStyle,
     ),
   ];
   int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(accentColor: Colors.cyan, fontFamily: 'Roboto'),
+        theme: ThemeData(accentColor: Colors.cyan, fontFamily: 'Roboto'),
         home: Scaffold(
-          
-      // appBar: AppBar(
-      //   title: Text('Badminton'),
-      // ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.cyan,
-        type: BottomNavigationBarType.fixed,
-        fixedColor: Colors.white,
-        iconSize: 30.0,
-        currentIndex: _selectedIndex,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.sports_handball,
-              ),
-              label: 'Courts'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), label: 'Events'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.attach_money), label: 'Records'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_pin_circle), label: 'Profile')
-        ],
-        onTap: _onItemTapped,
-        
-      ),
-    ));
+          // appBar: AppBar(
+          //   title: Text('Badminton'),
+          // ),
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.cyan,
+            type: BottomNavigationBarType.fixed,
+            fixedColor: Colors.white,
+            iconSize: 30.0,
+            currentIndex: _selectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.sports_handball,
+                  ),
+                  label: 'Courts'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_today), label: 'Events'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.attach_money), label: 'Records'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person_pin_circle), label: 'Profile')
+            ],
+            onTap: _onItemTapped,
+          ),
+        ));
   }
 
   void _onItemTapped(int index) {
